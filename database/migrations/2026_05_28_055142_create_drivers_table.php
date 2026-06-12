86ada33e16f06d0835e3465b180a3210c33cc2c7
mariooo-o <alexsamosir3@gmail.com>
2026-06-12 23:12:18 +0700

    fix driver CRUD dan role

--- database/migrations/2026_05_28_055142_create_drivers_table.php
+++ database/migrations/2026_05_28_055142_create_drivers_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->enum('status', [
                 'active', 
                 'inactive' 
-            ])->default('active');
+            ])->default('inactive');
             $table->timestamps();
         });
     }
